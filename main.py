abc9ceaebe6dfbcbd35d9702f6b2f704a197d9c8
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-02-27 20:07:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from flask_wtf import FlaskForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -6,10 +7,9 @@ from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SECRET_KEY'] = '12345678'
 
 # CREATE DATABASE
-
 
 class Base(DeclarativeBase):
     pass
@@ -38,8 +38,20 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == 'POST':
+        new_user = User(
+            email=request.form.get('email'),
+            name=request.form.get('name'),
+            password=request.form.get('password')
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return render_template("secrets.html", name=request.form.get('name'))
+
     return render_template("register.html")
 
 
@@ -58,10 +70,12 @@ def logout():
     pass
 
 
-@app.route('/download')
+@app.route('/download', methods=["GET", "POST"])
 def download():
-    pass
+    return send_from_directory('static', path="files/cheat_sheet.pdf")
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
