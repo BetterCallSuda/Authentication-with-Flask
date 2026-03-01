3d379ec5bbeee51624ad64e49861371b5abee507
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-01 19:23:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '12345678'
+app.config['SECRET_KEY'] = '102-537-606'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "login"
 
 # CREATE DATABASE
 
@@ -22,7 +27,8 @@ db.init_app(app)
 # CREATE TABLE IN DB
 
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
@@ -33,6 +39,13 @@ with app.app_context():
     db.create_all()
 
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -41,27 +54,51 @@ def home():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == 'POST':
-        new_user = User(
-            email=request.form.get('email'),
-            name=request.form.get('name'),
+        user = User.query.filter_by(
+            email=request.form.get('email')
+        ).first()
 
-            #Hashing the password to make it more authenticated!
-            password= generate_password_hash(request.form.get('password'), salt_length=8, method='pbkdf2:sha256')
+        flash("User already exist")
 
-        )
+        if not user:
+            new_user = User(
+                email=request.form.get('email'),
+                name=request.form.get('name'),
+                password= generate_password_hash(request.form.get('password'), salt_length=8, method='pbkdf2:sha256')
 
-        db.session.add(new_user)
-        db.session.commit()
+            )
 
-        return render_template("secrets.html", name=request.form.get('name'))
+            db.session.add(new_user)
+            db.session.commit()
+
+            login_user(new_user)
+
+            return render_template("secrets.html", name=request.form.get('name'))
 
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
 
+        user = User.query.filter_by(
+            email=request.form.get('email')
+        ).first()
+
+        if not user:
+            flash("User does not exist")
+            return redirect(url_for("login"))
+
+        if not check_password_hash(user.password, request.form.get("password")):
+            flash("Wrong password")
+            return redirect(url_for("login"))
+
+        login_user(user)
+        return render_template("secrets.html", name=user.name)
+
+    return render_template("login.html")
 
 @app.route('/secrets')
 def secrets():
@@ -70,7 +107,8 @@ def secrets():
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return render_template("login.html")
 
 
 @app.route('/download', methods=["GET", "POST"])
